d3cadccfa59ab4456a01faf47f542fd05956ed58
Z-dh <1332230995@qq.com>
2018-11-27 13:45:35 +0800

    feat:添加爬取视频封面图和视频地址

---
+++ server/tasks/trailer.js
@@ -0,0 +1,20 @@
+const cp = require('child_process')
+const {resolve} = require('path')
+;(async ()=>{
+  const  script = resolve(__dirname,'../crawler/video')
+  const child = cp.fork(script,[])
+  let invoked = false
+  child.on('error',err=>{
+    if (invoked) return
+    invoked = true
+  })
+  child.on('exit',code=>{
+    if (invoked) return
+    invoked=true
+    let err = code ===0?null:new Element("exit code " +code)
+    console.log(err)
+  })
+  child.on('message',data=>{
+    console.log(data)
+  })
+})()
